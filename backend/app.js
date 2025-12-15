fda533fb9bf847a4f9016b56972dedfb2ac5eaf9
Ishita-Singh28 <ishitasingh28516@gmail.com>
2025-12-15 13:43:24 +0530

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -4,20 +4,24 @@ const MongoStore = require('connect-mongo');
 const cors = require('cors');
 const helmet = require('helmet');
 const logger = require('./middleware/logger');
+
 const authRoutes = require('./routes/authRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const reportRoutes = require('./routes/reportRoutes');
 
 const app = express();
 
+
+app.set('trust proxy', 1);
+
 app.use(helmet());
 app.use(express.json());
 app.use(logger);
+
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: 'https://project-management-system-frontend-fwzi.onrender.com',
   credentials: true
 }));
-
 
 const sessionStore = MongoStore.create({
   mongoUrl: process.env.MONGO_URI,
@@ -25,14 +29,17 @@ const sessionStore = MongoStore.create({
 });
 
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'secret',
+  name: 'connect.sid',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   store: sessionStore,
-  cookie: { maxAge: 1000 * 60 * 60 * 24,
-    sameSite: 'none', 
-    secure: true }
-
+  cookie: {
+    httpOnly: true,
+    secure: true,      
+    sameSite: 'none',    
+    maxAge: 1000 * 60 * 60 * 24
+  }
 }));
 
 app.use('/api/auth', authRoutes);
